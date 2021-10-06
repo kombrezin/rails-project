ad2d9c62bcc702ddc9514e4f870649bfd5a9479b
kombrezin <kombreff@gmail.com>
2021-10-06 20:13:02 +0000

    login loggout

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,5 +1,6 @@
 class CarsController < ApplicationController
-  before_action :set_car, only: %i[ show edit update destroy ]
+before_action :authenticate_user!  
+before_action :set_car, only: %i[ show edit update destroy ]
 
   # GET /cars or /cars.json
   def index
